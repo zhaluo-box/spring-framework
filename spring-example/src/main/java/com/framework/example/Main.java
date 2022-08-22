385f6df3bf6929936598cf41e1637e6461e0c60a
zhaluobox <zhaluobox@163.com>
2022-08-22 21:35:04 +0800

    新建module spring example

---
+++ spring-example/src/main/java/com/framework/example/Main.java
@@ -0,0 +1,16 @@
+package com.framework.example;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Created  on 2022/8/22 21:21:27
+ *
+ * @author zl
+ */
+@Slf4j
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+}

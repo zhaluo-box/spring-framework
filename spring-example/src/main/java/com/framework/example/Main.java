30e4dd6b79cee29019bb27559de91b806f5b1336
zhaluobox <zhaluobox@163.com>
2022-08-22 21:36:45 +0800

    新建module spring example

--- spring-example/src/main/java/com/framework/example/Main.java
+++ spring-example/src/main/java/com/framework/example/Main.java
@@ -1,16 +1,17 @@
 package com.framework.example;
 
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
  * Created  on 2022/8/22 21:21:27
  *
  * @author zl
  */
-@Slf4j
+//@Slf4j
 public class Main {
 
 	public static void main(String[] args) {
-
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+		context.refresh();
 	}
 }

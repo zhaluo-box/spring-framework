d1a757694d5bd626443be80f241cbc726f430d40
zl <zhaluobox@163.com>
2023-08-31 15:33:08 +0800

    yaml 外部配置Bean学习

--- spring-example/src/test/java/com/framework/example/source/code/learn/bean/configmetadata/ConfigMetadataTest.java
+++ spring-example/src/test/java/com/framework/example/source/code/learn/bean/configmetadata/ConfigMetadataTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 
 import java.util.Map;
+import java.util.Properties;
 
 /**
  * 配置元信息测试
@@ -27,5 +28,26 @@ public class ConfigMetadataTest {
 		Map<String, User> beansOfType = beanFactory.getBeansOfType(User.class);
 		System.out.println("beansOfType = " + beansOfType);
 
+	}
+
+	/**
+	 * yml 配置bean  与 spring @configProperties 的联系
+	 * 由于 spring Beans 中 optional("org.yaml:snakeyaml")， 所以测试的时候需要手动添加依赖 compile('org.yaml:snakeyaml:2.0')
+	 */
+	@Test
+	@DisplayName("yml 配置Bean 测试")
+	public void yamlConfigBeanTest() {
+		// 创建 IoC 底层容器
+		DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+		// 创建 XML 资源的 BeanDefinitionReader
+		XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
+		// 记载 XML 资源
+		reader.loadBeanDefinitions("META-INF/yaml-property-source-context.xml");
+		// 获取 Map YAML 对象
+		Map<String, Object> yamlMap = beanFactory.getBean("yamlMap", Map.class);
+		System.out.println(yamlMap);
+
+		Properties yamlProperties = beanFactory.getBean("yamlProperties", Properties.class);
+		System.out.println("yamlProperties = " + yamlProperties);
 	}
 }
